184da3ae3d4321e16edebacce98df9b42b3f89df
aravindakshand <aravindakshan.d@thoughtworks.com>
2025-12-18 17:04:02 +0530

    email validation and delete-patient endpoint created

--- src/main/java/com/pm/patient_service/service/PatientService.java
+++ src/main/java/com/pm/patient_service/service/PatientService.java
@@ -8,6 +8,7 @@ import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -39,8 +40,10 @@ public class PatientService {
     public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
         Patient patient = patientRepository.findById(id)
                 .orElseThrow(() -> new PatientNotFoundException("Patient not found with ID: " + id));
-        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
-            throw new EmailAlreadyExistsException("A patient with this email " + patientRequestDTO.getEmail() + " already exists");
+        if (patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)) {
+            throw new EmailAlreadyExistsException(
+                    "A patient with this email " + patientRequestDTO.getEmail() + " already exists"
+            );
         }
 
         patient.setName(patientRequestDTO.getName());
@@ -50,5 +53,9 @@ public class PatientService {
 
         Patient updatedPatient = patientRepository.save(patient);
         return PatientMapper.toDTO(updatedPatient);
+    }
+
+    public void deletePatient(UUID id){
+        patientRepository.deleteById(id);
     }
 }
